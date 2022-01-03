d78db277c43954c27373de614e2c00e24a825bcd
klenQ <51435887+klenQ@users.noreply.github.com>
2022-01-03 16:03:44 +0800

    hello_react finished

---
+++ Staging/hello_react/src/components/Hello/Hello.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Hello extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello React Component!!!</h1>
+                
+            </div>
+        )
+    }
+}
